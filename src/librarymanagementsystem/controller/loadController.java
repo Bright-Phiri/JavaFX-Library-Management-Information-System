2f3266a9f4137d92209e2a503d0236c5c62ee3a6
Bright <Bright@10.1.101.84>
2021-01-31 07:06:06 +0200

    added controller functions

--- src/librarymanagementsystem/controller/loadController.java
+++ src/librarymanagementsystem/controller/loadController.java
@@ -33,15 +33,6 @@ public class loadController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        DatabaseConnection.checkTable("User", "CREATE TABLE `User` ( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `Firstname` TEXT, `Lastame` TEXT, `Username` TEXT, `Email` TEXT,`Password` TEXT, `Usertype` TEXT )");
-        DatabaseConnection.checkTable("Fee", "CREATE TABLE `Fee` (`BookID` TEXT, `BookName` TEXT, `StudentID` TEXT, `StudentName ` TEXT,`BorrowedDate` TEXT, `ReturnDate` TEXT,`LateFee` Double, `Username` TEXT,`Date` TEXT )");
-        DatabaseConnection.checkTable("Book", "CREATE TABLE Book ('BookID' TEXT, 'Name' TEXT, 'Author' TEXT, 'Publisher' TEXT, 'Edition' INTEGER, 'Quantity' INTEGER,'RemainingBooks' INTEGER Default 0, 'Availability' TEXT Default 'Available', 'Section' TEXT,PRIMARY KEY(BookID))");
-        DatabaseConnection.checkTable("Student", "CREATE TABLE Student ( `StudentID` TEXT, `Name` TEXT, `Email` TEXT, `Phone` TEXT, PRIMARY KEY(StudentID))");
-        DatabaseConnection.checkTable("ArhieveStudent", "CREATE TABLE ArhieveStudent ( `StudentID` TEXT, `Name` TEXT, `Email` TEXT, `Phone` TEXT, PRIMARY KEY(StudentID))");
-        DatabaseConnection.checkTable("IssueBook", "CREATE TABLE \"IssueBook\" ( `IssuedID` INTEGER PRIMARY KEY AUTOINCREMENT, `BookID` TEXT, `BookName` TEXT, `StudentID` TEXT, `StudentName` TEXT, `IssuedDate` Text, `ReturnDate` Text, `Days` Integer, `Fee` Double DEFAULT 0.00,`Notify` Text Default 'False',FOREIGN KEY(`BookID`) REFERENCES `Book`(`BookID`), FOREIGN KEY(`StudentID`) REFERENCES `Student`(`StudentID`) )");
-        DatabaseConnection.checkTable("ShortTermBook", "CREATE TABLE \"ShortTermBook\" ( `IssuedID` INTEGER PRIMARY KEY AUTOINCREMENT, `BookID` TEXT, `BookName` TEXT, `StudentID` TEXT, `StudentName` TEXT, `IssuedTime` Text, `ReturnTime` Text, `Hours` Integer, `Fee` Double DEFAULT 0.00,`Notify` Text Default 'False',FOREIGN KEY(`BookID`) REFERENCES `Book`(`BookID`), FOREIGN KEY(`StudentID`) REFERENCES `Student`(`StudentID`) )");
-        DatabaseConnection.checkTable("Account", "CREATE TABLE Account ( `ID` Integer default 1,`LateFeePerDay` Double,`LateFeePerHour` Double)");
-        DatabaseConnection.checkTable("MailServer", "CREATE TABLE `MailServer` ( `ServerName` TEXT, `ServerPort` INTEGER, `Email` TEXT, `Password` TEXT, `Authentication` TEXT, `TLS` TEXT )");
 
         Tooltip closeApp = new Tooltip("Close");
         closeApp.setStyle("-fx-font-size:11");
