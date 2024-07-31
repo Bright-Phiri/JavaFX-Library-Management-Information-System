3fdfe9baa537e2e477afb4499a407e5ec6411b08
Bright Issah <bphiri.aki@gmail.com>
2024-07-31 08:04:39 +0200

    add create system tables

--- src/librarymanagementsystem/controller/loadController.java
+++ src/librarymanagementsystem/controller/loadController.java
@@ -33,7 +33,15 @@ public class loadController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
+        DatabaseConnection.checkTable("User", "CREATE TABLE `library`.`user` ( `ID` INT NOT NULL AUTO_INCREMENT , `FirstName` TEXT NULL , `LastName` TEXT NULL , `Username` TEXT NOT NULL , `Email` TEXT NOT NULL , `Password` TEXT NULL , `Usertype` TEXT NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB; ");
+        DatabaseConnection.checkTable("Fee", "CREATE TABLE `library`.`fee` ( `BookID` TEXT NOT NULL , `BookName` TEXT NOT NULL , `StudentID` TEXT NOT NULL , `StudentName` TEXT NOT NULL , `BorrowedDate` TEXT NOT NULL , `ReturnDate` TEXT NOT NULL , `LateFee` DOUBLE NOT NULL , `Username` TEXT NOT NULL , `Date` TEXT NOT NULL ) ENGINE = InnoDB; ");
+        DatabaseConnection.checkTable("Book", "CREATE TABLE `library`.`book` ( `BookID` TEXT NOT NULL , `Name` TEXT NOT NULL , `Author` TEXT NOT NULL , `Publisher` TEXT NOT NULL , `Edition` INT NOT NULL , `Quantity` INT NOT NULL , `RemainingBooks` INT NULL DEFAULT '0' , `Availability` TEXT NULL DEFAULT 'Available' , `Section` TEXT NOT NULL) ENGINE = InnoDB;");
+        DatabaseConnection.checkTable("Student", "CREATE TABLE `library`.`student` ( `StudentID` TEXT NOT NULL , `Name` TEXT NOT NULL , `Email` TEXT NOT NULL , `Phone` TEXT NOT NULL ) ENGINE = InnoDB; ");
+        DatabaseConnection.checkTable("ArhieveStudent", "CREATE TABLE `library`.`arhieveStudent` ( `StudentID` TEXT NOT NULL , `Name` TEXT NOT NULL , `Email` TEXT NOT NULL , `Phone` TEXT NOT NULL ) ENGINE = InnoDB; ");
+        DatabaseConnection.checkTable("IssueBook", "CREATE TABLE `library`.`issuebook` ( `IssuedID` INT NOT NULL AUTO_INCREMENT , `BookID` TEXT NOT NULL , `BookName` TEXT NOT NULL , `StudentID` TEXT NOT NULL , `StudentName` TEXT NOT NULL , `IssuedDate` TEXT NOT NULL , `ReturnDate` TEXT NOT NULL , `Days` INT NOT NULL , `Fee` DOUBLE NULL , `Notify` TEXT NOT NULL DEFAULT 'False' , PRIMARY KEY (`IssuedID`)) ENGINE = InnoDB;");
+        DatabaseConnection.checkTable("ShortTermBook", "CREATE TABLE `library`.`shorttermbook` ( `IssuedID` INT NOT NULL AUTO_INCREMENT , `BookID` TEXT NOT NULL , `BookName` TEXT NOT NULL , `StudentID` TEXT NOT NULL , `StudentName` TEXT NOT NULL , `IssuedTime` TEXT NOT NULL , `ReturnTime` TEXT NOT NULL , `Hours` INT NOT NULL , `Fee` DOUBLE NULL , `Notify` TEXT NOT NULL DEFAULT 'False' , PRIMARY KEY (`IssuedID`)) ENGINE = InnoDB;");
+        DatabaseConnection.checkTable("Account", "CREATE TABLE `library`.`account` ( `ID` INT NOT NULL DEFAULT '1' , `LateFeePerDay` DOUBLE NOT NULL , `LateFeePerHour` DOUBLE NOT NULL ) ENGINE = InnoDB; ");
+        DatabaseConnection.checkTable("MailServer", "CREATE TABLE `library`.`mailserver` ( `ServerName` TEXT NOT NULL , `ServerPort` INT NOT NULL , `Email` TEXT NOT NULL , `Password` TEXT NOT NULL , `Authentication` TEXT NOT NULL , `TLS` TEXT NOT NULL ) ENGINE = InnoDB; ");
         Tooltip closeApp = new Tooltip("Close");
         closeApp.setStyle("-fx-font-size:11");
         closeApp.setMinSize(20, 20);
